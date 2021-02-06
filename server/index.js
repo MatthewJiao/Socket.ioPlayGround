45955c8161a913c7d7a5b28c01101e6b39832a57
Matthew Jiao <jiaomatthew222@gmail.com>
2021-02-05 23:04:09 -0500

    user functions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js')
  
 const PORT = process.env.PORT || 5000
 const router = require('./router')
@@ -10,16 +12,13 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 io.on('connection', (socket) => {
-    console.log('we have a new connection')
 
     socket.on('join', ({name, room}, callback) => {
-        console.log(name, room)
+        const {error, user} = addUser({id: socket.id, name, room})
+        
+        if (error) return callback(error)
 
-       // const error = true
-
-        //if (error) {
-          //  callback({error: 'error'})
-        //}
+        socket.join(user.room)
     })
 
     socket.on('disconnect', () => {
